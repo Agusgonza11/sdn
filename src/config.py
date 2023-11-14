e51238c2a3faca774b89bf4096dc91ad7bef2208
Agustin Gonzalez <aguselgonza@gmail.com>
2023-11-14 20:30:41 -0300

    add config file

---
+++ src/config.py
@@ -0,0 +1,21 @@
+import ConfigParser
+
+def read_configuration(config_file):
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+
+    if 'Config' in config.sections():
+        return config.items('Config')
+
+
+def get_configuration_values(config_file='config.ini'):
+    config_items = read_configuration(config_file)
+    configuration = dict(config_items)
+
+    return {
+        'protocol': configuration.get('protocol'),
+        'src_host': configuration.get('src_host'),
+        'dst_host': configuration.get('dst_host'),
+        'src_port': configuration.get('src_port'),
+        'dst_port': configuration.get('dst_port')
+    }
