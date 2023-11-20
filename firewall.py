fc423c2e7a1002de0b477c48212a235643d8ccce
Agustin Gonzalez <aguselgonza@gmail.com>
2023-11-20 14:50:49 -0300

    nuevo firewall

--- firewall.py
+++ firewall.py
@@ -9,6 +9,8 @@ import os
 import argparse
 
 log = core.getLogger()
+log.setLevel("DEBUG")
+
 # Add your global variables here ...
 
 TCP = 6
@@ -20,38 +22,39 @@ class Firewall(EventMixin):
         log.debug("Enabling Firewall Module")
         self.rules = get_configuration_values()
         self.switch_id = switch_id
+        log.debug("Firewall initialized for switch %s", switch_id)
 
     def _handle_ConnectionUp(self, event):
         log.debug("The switch %s is connected", event.dpid)
 
     def _handle_PacketIn(self, event):
-        if self.switch_id is not None and self.switch_id != event.dpid:
+        if self.switch_id != event.dpid:
             return
         packet = event.parsed
-        print(f"The packet was {packet}")
-        log.debug("Received packet: %s", packet)  # Esta línea imprime el paquete completo
-
+        log.debug("El paquete que llego es %s ", packet)
         if packet.type == packet.IP_TYPE:
             ip_packet = packet.find("ipv4")
-            protocol = ip_packet.protocol
-            src_host = self.get_host_from_ip(ip_packet.srcip)
-            dst_host = self.get_host_from_ip(ip_packet.dstip)
-            src_ip = ip_packet.srcip
-            dst_ip = ip_packet.dstip
-            transport_packet = None
-            if ip_packet.protocol == TCP:
-                transport_packet = ip_packet.find("tcp")
-            elif ip_packet.protocol == UDP:
-                transport_packet = ip_packet.find("udp")
-            if transport_packet:
-                src_port = transport_packet.srcport
-                dst_port = transport_packet.dstport
-                self.filter_packet(event, protocol, src_ip, dst_ip, src_port, dst_port, src_host, dst_host)
+            if ip_packet:
+                log.debug("El paquete ip que llego es %s ", ip_packet)
+                if ip_packet.protocol == TCP:
+                    transport_packet = ip_packet.find("tcp")
+                elif ip_packet.protocol == UDP:
+                    transport_packet = ip_packet.find("udp")
+                else:
+                    transport_packet = None
 
+                if transport_packet:
+                    src_ip = ip_packet.srcip
+                    dst_ip = ip_packet.dstip
+                    src_port = transport_packet.srcport
+                    dst_port = transport_packet.dstport
+                    src_host = self.get_host_from_ip(src_ip, event.topology)
+                    dst_host = self.get_host_from_ip(dst_ip, event.topology)
 
+                    self.filter_packet(event, ip_packet.protocol, src_ip, dst_ip, src_port, dst_port, src_host, dst_host)
 
-    def get_host_from_ip(self, ip_address):
-        for host_number, ip_addresses in self.topology.hosts.items():
+    def get_host_from_ip(self, ip_address, topology):
+        for host_number, ip_addresses in topology.hosts.items():
             if ip_address in ip_addresses:
                 return host_number
         return None
@@ -71,6 +74,8 @@ class Firewall(EventMixin):
             return
 
     def drop_packet(self, event):
+        log.debug("Entra a romper el paquete")
+
         msg = of.ofp_packet_out()
         msg.buffer_id = event.ofp.buffer_id
         msg.in_port = event.port
